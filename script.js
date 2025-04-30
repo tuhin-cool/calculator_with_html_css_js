cc48a4ac2670bf5da41fabd7652e544efbfd41db
Md_Tuhin <tufan506070@gmail.com>
2025-04-30 19:24:34 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// script.js
+function appendValue(value) {
+    document.getElementById("display").value += value;
+  }
+  
+  function clearDisplay() {
+    document.getElementById("display").value = "";
+  }
+  
+  function calculate() {
+    try {
+      const result = eval(document.getElementById("display").value);
+      document.getElementById("display").value = result;
+    } catch (e) {
+      document.getElementById("display").value = "Error";
+    }
+  }
+  
